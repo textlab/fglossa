a8e0d72e0b7a6e9d064eb20c0814e921062631b0
Anders Nøklestad <anders.noklestad@iln.uio.no>
2021-01-27 23:17:02 +0100

    Some dummy client code

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -4,109 +4,71 @@ open Elmish
 open Fable.Remoting.Client
 open Shared
 
-type Model =
-    { Todos: Todo list
-      Input: string }
+type Model = { Corpora: Corpus list }
 
 type Msg =
-    | GotTodos of Todo list
-    | SetInput of string
-    | AddTodo
-    | AddedTodo of Todo
+    | FetchCorpora
+    | FetchedCorpora of Corpus list
 
-let todosApi =
-    Remoting.createApi()
+let serverApi =
+    Remoting.createApi ()
     |> Remoting.withRouteBuilder Route.builder
-    |> Remoting.buildProxy<ITodosApi>
+    |> Remoting.buildProxy<IServerApi>
 
-let init(): Model * Cmd<Msg> =
-    let model =
-        { Todos = []
-          Input = "" }
-    let cmd = Cmd.OfAsync.perform todosApi.getTodos () GotTodos
+let init (): Model * Cmd<Msg> =
+    let model = { Corpora = [] }
+
+    let cmd =
+        Cmd.OfAsync.perform serverApi.getCorpora () FetchedCorpora
+
     model, cmd
 
 let update (msg: Msg) (model: Model): Model * Cmd<Msg> =
     match msg with
-    | GotTodos todos ->
-        { model with Todos = todos }, Cmd.none
-    | SetInput value ->
-        { model with Input = value }, Cmd.none
-    | AddTodo ->
-        let todo = Todo.create model.Input
-        let cmd = Cmd.OfAsync.perform todosApi.addTodo todo AddedTodo
-        { model with Input = "" }, cmd
-    | AddedTodo todo ->
-        { model with Todos = model.Todos @ [ todo ] }, Cmd.none
+    | FetchCorpora -> model, Cmd.OfAsync.perform serverApi.getCorpora () FetchedCorpora
+    | FetchedCorpora corpora -> { model with Corpora = corpora }, Cmd.none
 
 open Fable.React
 open Fable.React.Props
 open Fulma
 
 let navBrand =
-    Navbar.Brand.div [ ] [
-        Navbar.Item.a [
-            Navbar.Item.Props [ Href "https://safe-stack.github.io/" ]
-            Navbar.Item.IsActive true
-        ] [
-            img [
-                Src "/favicon.png"
-                Alt "Logo"
+    Navbar.Brand.div [] [
+        Navbar.Item.a [ Navbar.Item.Props [ Href "https://safe-stack.github.io/" ]
+                        Navbar.Item.IsActive true ] [
+            img [ Src "/favicon.png"; Alt "Logo" ]
+        ]
+    ]
+
+let containerBox (model: Model) (dispatch: Msg -> unit) =
+    Box.box' [] [
+        Content.content [] [
+            Content.Ol.ol [] [
+                for corpus in model.Corpora do
+                    li [] [ str corpus.Name ]
             ]
         ]
     ]
 
-let containerBox (model : Model) (dispatch : Msg -> unit) =
-    Box.box' [ ] [
-        Content.content [ ] [
-            Content.Ol.ol [ ] [
-                for todo in model.Todos do
-                    li [ ] [ str todo.Description ]
-            ]
-        ]
-        Field.div [ Field.IsGrouped ] [
-            Control.p [ Control.IsExpanded ] [
-                Input.text [
-                  Input.Value model.Input
-                  Input.Placeholder "What needs to be done?"
-                  Input.OnChange (fun x -> SetInput x.Value |> dispatch) ]
-            ]
-            Control.p [ ] [
-                Button.a [
-                    Button.Color IsPrimary
-                    Button.Disabled (Todo.isValid model.Input |> not)
-                    Button.OnClick (fun _ -> dispatch AddTodo)
-                ] [
-                    str "Add"
-                ]
-            ]
-        ]
-    ]
-
-let view (model : Model) (dispatch : Msg -> unit) =
-    Hero.hero [
-        Hero.Color IsPrimary
-        Hero.IsFullHeight
-        Hero.Props [
-            Style [
-                Background """linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("https://unsplash.it/1200/900?random") no-repeat center center fixed"""
-                BackgroundSize "cover"
-            ]
-        ]
-    ] [
-        Hero.head [ ] [
-            Navbar.navbar [ ] [
-                Container.container [ ] [ navBrand ]
+let view (model: Model) (dispatch: Msg -> unit) =
+    Hero.hero [ Hero.Color IsPrimary
+                Hero.IsFullHeight
+                Hero.Props [ Style [ Background
+                                         """linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("https://unsplash.it/1200/900?random") no-repeat center center fixed"""
+                                     BackgroundSize "cover" ] ] ] [
+        Hero.head [] [
+            Navbar.navbar [] [
+                Container.container [] [ navBrand ]
             ]
         ]
 
-        Hero.body [ ] [
-            Container.container [ ] [
-                Column.column [
-                    Column.Width (Screen.All, Column.Is6)
-                    Column.Offset (Screen.All, Column.Is3)
-                ] [
-                    Heading.p [ Heading.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ] [ str "fglossa" ]
+        Hero.body [] [
+            Container.container [] [
+                Column.column [ Column.Width(Screen.All, Column.Is6)
+                                Column.Offset(Screen.All, Column.Is3) ] [
+                    Heading.p [ Heading.Modifiers [ Modifier.TextAlignment(Screen.All, TextAlignment.Centered) ] ] [
+                        str "fglossa"
+                    ]
                     containerBox model dispatch
                 ]
             ]
