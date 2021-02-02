24146d1b91f72b2eff27c3904bb944f200c56620
Anders Nøklestad <anders.noklestad@iln.uio.no>
2021-02-02 01:14:34 +0100

    WIP on metadata categories and menus

--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -11,15 +11,48 @@ type TextEncoding =
     | Latin1
 
 module Metadata =
+    /// Base class for all metadata categories
+    [<AbstractClass>]
+    type Category(aName: string) =
+        member _.Name = aName
+        abstract member Code: string
+
+    /// Metadata category that can be presented as a metadata value list
+    [<AbstractClass>]
+    type StringCategory(aName) =
+        inherit Category(aName)
+
+    /// Metadata category that can be presented as a metadata value list (like StringCategory),
+    /// but that will be sorted numerically.
+    /// Can also be used with an interval selector.
+    [<AbstractClass>]
+    type NumberCategory(aName) =
+        inherit Category(aName)
+
+    /// Metadata category that can be used with a free text search input.
+    [<AbstractClass>]
+    type LongTextCategory(aName) =
+        inherit Category(aName)
+
+    type MenuState =
+        | Open
+        | Closed
+
+    type MenuItem =
+        | StringSelect of StringCategory
+        | NumberSelect of NumberCategory
+        | Interval of NumberCategory
+        | FreeTextSearch of LongTextCategory
+        // Since a section may have no title, the proper F# way would be to define it
+        // as a "string option", but using a normal string (and simply omitting the title
+        // bar if the title is empty) makes it easier for non-programmers to define menus.
+        | Section of title: string * MenuState * children: MenuItem list
+
+    type Menu = MenuItem list
+
+    type Selection = (string * string list) list
+
     type Value = string * string
-
-    type Category =
-        | Strings of code: string * name: string * values: Value []
-        | Interval of code: string * name: string * from: int * ``to``: int
-        | FreeText of code: string * name: string * value: string
-        | Section of title: string option * isOpen: bool * children: Category list
-
-    type Selection = Category []
 
 type Corpus =
     { Code: string
