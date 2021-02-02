7339b5131eecf57812ff6da2200efe1849ec2d12
Anders Nøklestad <anders.noklestad@iln.uio.no>
2021-02-02 02:12:12 +0100

    Formatting

--- build.fsx
+++ build.fsx
@@ -21,13 +21,14 @@ let npm args workingDir =
         match ProcessUtils.tryFindFileOnPath "npm" with
         | Some path -> path
         | None ->
-            "npm was not found in path. Please install it and make sure it's available from your path. " +
-            "See https://safe-stack.github.io/docs/quickstart/#install-pre-requisites for more info"
+            "npm was not found in path. Please install it and make sure it's available from your path. "
+            + "See https://safe-stack.github.io/docs/quickstart/#install-pre-requisites for more info"
             |> failwith
 
-    let arguments = args |> String.split ' ' |> Arguments.OfArgs
+    let arguments =
+        args |> String.split ' ' |> Arguments.OfArgs
 
-    Command.RawCommand (npmPath, arguments)
+    Command.RawCommand(npmPath, arguments)
     |> CreateProcess.fromCommand
     |> CreateProcess.withWorkingDirectory workingDir
     |> CreateProcess.ensureExitCode
@@ -35,64 +36,72 @@ let npm args workingDir =
     |> ignore
 
 let dotnet cmd workingDir =
-    let result = DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
-    if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" cmd workingDir
+    let result =
+        DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
+
+    if result.ExitCode <> 0 then
+        failwithf "'dotnet %s' failed in %s" cmd workingDir
 
 Target.create "Clean" (fun _ -> Shell.cleanDir deployDir)
 
 Target.create "InstallClient" (fun _ -> npm "install" ".")
 
-Target.create "Bundle" (fun _ ->
-    dotnet (sprintf "publish -c Release -o \"%s\"" deployDir) serverPath
-    npm "run build" "."
-)
+Target.create
+    "Bundle"
+    (fun _ ->
+        dotnet (sprintf "publish -c Release -o \"%s\"" deployDir) serverPath
+        npm "run build" ".")
 
-Target.create "Azure" (fun _ ->
-    let web = webApp {
-        name "fglossa"
-        zip_deploy "deploy"
-    }
-    let deployment = arm {
-        location Location.WestEurope
-        add_resource web
-    }
+Target.create
+    "Azure"
+    (fun _ ->
+        let web =
+            webApp {
+                name "fglossa"
+                zip_deploy "deploy"
+            }
 
-    deployment
-    |> Deploy.execute "fglossa" Deploy.NoParameters
-    |> ignore
-)
+        let deployment =
+            arm {
+                location Location.WestEurope
+                add_resource web
+            }
 
-Target.create "Run" (fun _ ->
-    dotnet "build" sharedPath
-    [ async { dotnet "watch run" serverPath }
-      async { npm "run start" "." } ]
-    |> Async.Parallel
-    |> Async.RunSynchronously
-    |> ignore
-)
+        deployment
+        |> Deploy.execute "fglossa" Deploy.NoParameters
+        |> ignore)
 
-Target.create "RunTests" (fun _ ->
-    dotnet "build" sharedTestsPath
-    [ async { dotnet "watch run" serverTestsPath }
-      async { npm "run test:live" "." } ]
-    |> Async.Parallel
-    |> Async.RunSynchronously
-    |> ignore
-)
+Target.create
+    "Run"
+    (fun _ ->
+        dotnet "build" sharedPath
+
+        [ async { dotnet "watch run" serverPath }
+          async { npm "run start" "." } ]
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> ignore)
+
+Target.create
+    "RunTests"
+    (fun _ ->
+        dotnet "build" sharedTestsPath
+
+        [ async { dotnet "watch run" serverTestsPath }
+          async { npm "run test:live" "." } ]
+        |> Async.Parallel
+        |> Async.RunSynchronously
+        |> ignore)
 
 open Fake.Core.TargetOperators
 
 "Clean"
-    ==> "InstallClient"
-    ==> "Bundle"
-    ==> "Azure"
+==> "InstallClient"
+==> "Bundle"
+==> "Azure"
 
-"Clean"
-    ==> "InstallClient"
-    ==> "Run"
+"Clean" ==> "InstallClient" ==> "Run"
 
-"Clean"
-    ==> "InstallClient"
-    ==> "RunTests"
+"Clean" ==> "InstallClient" ==> "RunTests"
 
 Target.runOrDefaultWithArguments "Bundle"
