ce3745c79b689c7a8a6c995461c57cca82ec7c1c
Anders Nøklestad <anders.noklestad@iln.uio.no>
2021-03-03 03:24:26 +0100

    Refactor

--- src/Client/fs/Metadata.fs
+++ src/Client/fs/Metadata.fs
@@ -1,9 +1,39 @@
 module Metadata
 
+open Fable.Core.JsInterop
 open Feliz
 open Feliz.Bulma
 open Shared.Metadata
 open Model
+
+let FixedSizeList: obj = importMember "react-window"
+
+let ListItem (props: {| index: int; style: obj |}) =
+    Html.div [ prop.style [ style.height (int props.style?height)
+                            style.left (int props.style?left)
+                            style.position.absolute
+                            style.top (int props.style?top)
+                            style.width (length.percent 100) ]
+               prop.text props.index ]
+
+// The components in react-window expect a `children` prop, which should be a
+// component function or class. However, all React functions in Fable.React and
+// Feliz (such as `ofImport`, `ofType`, and even `Fable.React.ReactBindings.React.createElement`)
+// seem to automatically call createElement on (i.e. instantiate) whatever components we pass
+// as their `children` arguments.
+// Hence, the only solution I have found is to import React ourselves and and call
+// React.createElement directly, since it allows us to provide function components as
+// the `children` argument and leaves them uninstantiated.
+
+let ReactBare: obj = importAll "react"
+
+let fixedSizeList: ReactElement =
+    ReactBare?createElement (FixedSizeList,
+                             createObj [ "height" ==> 100
+                                         "itemCount" ==> 10
+                                         "itemSize" ==> 35
+                                         "width" ==> 100 ],
+                             ListItem)
 
 let stringSelect (category: StringCategory) =
     Html.li (
@@ -28,8 +58,6 @@ let freeTextSearch (category: LongTextCategory) =
         Html.a [ prop.key category.Code
                  prop.text category.Name ]
     )
-
-module RW = Import.ReactWindow
 
 [<ReactComponent>]
 let Section
@@ -66,7 +94,7 @@ let Section
                               prop.style [ style.borderLeft (1, borderStyle.solid, "#dbdbdb") ]
                               prop.children children ]
 
-                    RW.fixedSizeList ]
+                    fixedSizeList ]
 
 let menu (model: LoadedCorpusModel) dispatch =
     let menuItems =
