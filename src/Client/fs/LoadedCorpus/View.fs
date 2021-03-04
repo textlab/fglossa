ca6b13b2d77babf587313a24450f4698b8a6c7ee
Anders Nøklestad <anders.noklestad@iln.uio.no>
2021-03-04 20:06:45 +0100

    Use utility class for button group

--- src/Client/fs/LoadedCorpus/View.fs
+++ src/Client/fs/LoadedCorpus/View.fs
@@ -7,9 +7,8 @@ open Model
 open LoadedCorpus.Update
 
 let topRowButtons =
-    Bulma.field.div [ field.isGrouped
-                      prop.children [ Bulma.control.div [ Bulma.button.button [ prop.text "Hide filters" ] ]
-                                      Bulma.control.div [ Bulma.button.button [ color.isInfo
-                                                                                prop.text "Reset form" ] ] ] ]
+    Bulma.buttons [ Bulma.button.button [ prop.text "Hide filters" ]
+                    Bulma.button.button [ color.isInfo
+                                          prop.text "Reset form" ] ]
 
 let corpusStartPage (model: LoadedCorpusModel) (dispatch: Msg -> unit) = topRowButtons
