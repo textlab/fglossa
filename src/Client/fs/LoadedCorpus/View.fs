26654b11d0a71eed0a32acb557da04cf14968f61
Anders Nøklestad <anders.noklestad@iln.uio.no>
2021-03-04 19:51:43 +0100

    Add top-row buttons (non-functional for now)

--- src/Client/fs/LoadedCorpus/View.fs
+++ src/Client/fs/LoadedCorpus/View.fs
@@ -6,4 +6,10 @@ open Feliz.Bulma
 open Model
 open LoadedCorpus.Update
 
-let corpusStartPage (model: LoadedCorpusModel) (dispatch: Msg -> unit) = Html.span "hei"
+let topRowButtons =
+    Bulma.field.div [ field.isGrouped
+                      prop.children [ Bulma.control.div [ Bulma.button.button [ prop.text "Hide filters" ] ]
+                                      Bulma.control.div [ Bulma.button.button [ color.isInfo
+                                                                                prop.text "Reset form" ] ] ] ]
+
+let corpusStartPage (model: LoadedCorpusModel) (dispatch: Msg -> unit) = topRowButtons
