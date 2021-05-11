4d7c1a9700c90817a83b125503a4d152b9279d11
Anders Nøklestad <anders.noklestad@iln.uio.no>
2021-05-11 09:06:44 +0200

    Handle errors

--- src/Server/fs/Remoting/Search.fs
+++ src/Server/fs/Remoting/Search.fs
@@ -44,21 +44,20 @@ let searchCorpus (connStr: string) (logger: ILogger) (searchParams: SearchParams
                 conn.Close()
 
                 return lastInsertId
-            with e ->
-                printfn $"{e}"
-                return raise e
+            with e -> return raise e
 
         }
 
-
     task {
-        let cqpProcs = runCmdWithOutput "pgrep" "-f cqp"
+        let cqpProcs =
+            Process.runCmdWithOutput "pgrep" "-f cqp"
+
         let nCqpProcs = cqpProcs.Split('\n').Length
 
         let corpus =
             Corpora.Server.getCorpus searchParams.CorpusCode
 
-        // If we are searching a corpus that does parallel search with multiple cpus and
+        // If we are searching in a corpus that does parallel search with multiple cpus and
         // this is the first search step, we check that we don't already exceed the max number
         // of CQP processes before starting the search. If we are at step 2 or 3, we should finish
         // what we started. Corpora that don't use multiple cpus are assumed to be small and
@@ -70,6 +69,28 @@ let searchCorpus (connStr: string) (logger: ILogger) (searchParams: SearchParams
             let searchId =
                 searchParams.SearchId
                 |> Option.defaultWith (fun () -> createSearch().Result)
+
+            let (output, error) =
+                [ "NDC;"
+                  "\"sier\";"
+                  "cat Last 0 10;" ]
+                |> String.concat "\n"
+                |> Process.runCmdWithInputOutputAndError "docker" "exec -i cwb cqp -c"
+
+            let isUndumpError =
+                Regex.IsMatch(error, "(?i)CQP Error:\s+Format error in undump file")
+
+            if (not isUndumpError) && (error.Length > 0) then
+                logger.Error(error)
+
+            // Split into lines and throw away the first line, which contains the CQP version.
+            // If counting? is true (which it is when we are searching, but not when retrieving
+            // results), the first line after that contains the number of results. Any following
+            // lines contain actual search results (only in the first step).
+            let results = output.Split('\n') |> Array.tail
+
+            printfn $"OUTPUT: {output}"
+            printfn $"ERROR LENGTH: {error.Length}"
 
             return
                 { Count = 1u
