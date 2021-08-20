f5d20911d59cd8d5b27813784625147cda456fd9
Anders Nøklestad <anders.noklestad@iln.uio.no>
2021-08-20 19:04:57 +0200

    WIP on CWB extended view

--- src/Client/fs/SearchViews/Cwb/Extended.fs
+++ src/Client/fs/SearchViews/Cwb/Extended.fs
@@ -35,37 +35,48 @@ let view (corpus: Corpus) (search: Search) (dispatch: Msg -> unit) =
         | Written -> "Sentence"
 
     let termView index term =
-        Bulma.column [ Bulma.field.div [ field.hasAddons
-                                         if query.Terms.Length > 1 then
-                                             field.hasAddonsRight
-                                         prop.children [ Bulma.control.div [ prop.children [ Bulma.button.button [ Bulma.icon [ Html.i [ prop.className
-                                                                                                                                             "fas fa-list" ] ] ] ] ]
-                                                         Bulma.control.div [ prop.children [ Bulma.button.button [ Bulma.icon [ Html.i [ prop.className
-                                                                                                                                             "fas fa-chevron-down" ] ] ] ] ]
-                                                         Bulma.control.div [ prop.children [ Bulma.input.text [ prop.style [ style.width
-                                                                                                                                 108 ] ] ] ]
-                                                         if query.Terms.Length > 1 then
-                                                             Bulma.control.div [ prop.children [ Bulma.button.button [ Bulma.icon [ Html.i [ prop.className
-                                                                                                                                                 "fas fa-minus" ] ] ] ] ] ] ]
-                       Bulma.field.div [ field.isGrouped
-                                         field.isGroupedMultiline
-                                         prop.children [ if hasLemma then
-                                                             checkbox "Lemma" "Lemma"
-                                                         checkbox "Start" "Start of word"
-                                                         checkbox "End" "End of word"
-                                                         checkbox "Middle" "Middle of word"
-                                                         if hasOrig then
-                                                             checkbox "Original" "Original form"
-                                                         if index = 0 then
-                                                             checkbox $"{segmentType} initial" $"{segmentType} initial"
-                                                         elif index = query.Terms.Length - 1 then
-                                                             // TODO: Add optional sentence final punctuation to query
-                                                             // to make this work in written text as well
-                                                             match corpus.Config.Modality with
-                                                             | Spoken ->
-                                                                 checkbox $"{segmentType} final" $"{segmentType} final"
-                                                             | Written -> Html.none ] ] ]
+        [ if index > 0 then
+              Bulma.column [ Bulma.field.div [ Bulma.input.text [] ] ]
+          Bulma.column [ Bulma.field.div [ field.hasAddons
+                                           if query.Terms.Length > 1 then
+                                               field.hasAddonsRight
+                                           prop.children [ Bulma.control.div [ prop.children [ Bulma.button.button [ Bulma.icon [ Html.i [ prop.className
+                                                                                                                                               "fas fa-list" ] ] ] ] ]
+                                                           Bulma.control.div [ prop.children [ Bulma.button.button [ Bulma.icon [ Html.i [ prop.className
+                                                                                                                                               "fas fa-chevron-down" ] ] ] ] ]
+                                                           Bulma.control.div [ prop.children [ Bulma.input.text [ prop.style [ style.width
+                                                                                                                                   108 ] ] ] ]
+                                                           if query.Terms.Length > 1 then
+                                                               Bulma.control.div [ prop.children [ Bulma.button.button [ Bulma.icon [ Html.i [ prop.className
+                                                                                                                                                   "fas fa-minus" ] ] ] ] ] ] ]
+                         Bulma.field.div [ field.isGrouped
+                                           field.isGroupedMultiline
+                                           prop.children [ if hasLemma then
+                                                               checkbox "Lemma" "Lemma"
+                                                           checkbox "Start" "Start of word"
+                                                           checkbox "End" "End of word"
+                                                           checkbox "Middle" "Middle of word"
+                                                           if hasOrig then
+                                                               checkbox "Original" "Original form"
+                                                           if index = 0 then
+                                                               checkbox
+                                                                   $"{segmentType} initial"
+                                                                   $"{segmentType} initial"
+                                                           elif index = query.Terms.Length - 1 then
+                                                               // TODO: Add optional sentence final punctuation to query
+                                                               // to make this work in written text as well
+                                                               match corpus.Config.Modality with
+                                                               | Spoken ->
+                                                                   checkbox
+                                                                       $"{segmentType} final"
+                                                                       $"{segmentType} final"
+                                                               | Written -> Html.none ] ] ]
+
+
+          ]
 
     Bulma.columns [ prop.style [ style.marginBottom 30 ]
-                    prop.children [ yield! query.Terms |> Array.mapi termView
+                    prop.children [ yield!
+                                        [ for (index, term) in query.Terms |> Array.indexed do
+                                              yield! termView index term ]
                                     Bulma.column [ Bulma.field.div [] ] ] ]
