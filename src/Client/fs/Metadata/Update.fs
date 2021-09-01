00da8c418db0276861a0b2e06c2151745c84ddd2
Anders Nøklestad <anders.noklestad@iln.uio.no>
2021-09-01 23:15:12 +0200

    Reorder update cases

--- src/Client/fs/Metadata/Update.fs
+++ src/Client/fs/Metadata/Update.fs
@@ -5,14 +5,41 @@ open Shared
 open Model
 
 type Msg =
-    | SelectItem of Metadata.Category * Metadata.StringSelectOption
-    | ToggleExclude of category: Metadata.Category
     | ToggleMetadataMenuOpen of category: Metadata.Category
-    | ToggleShowSelectionOpen
+    | ToggleExclude of category: Metadata.Category
+    | SelectItem of Metadata.Category * Metadata.StringSelectOption
     | DeselectItem of Metadata.Category * Metadata.StringSelectOption
+    | ToggleShowSelectionOpen
 
 let update (msg: Msg) (model: LoadedCorpusModel) : LoadedCorpusModel * Cmd<Msg> =
     match msg with
+    | ToggleMetadataMenuOpen category ->
+        let newCode =
+            if model.OpenMetadataCategoryCode = Some category.Code then
+                None
+            else
+                Some category.Code
+
+        { model with
+              OpenMetadataCategoryCode = newCode },
+        Cmd.none
+    | ToggleExclude category ->
+        let newMetadataSelection =
+            model.Search.MetadataSelection
+            |> Map.change
+                category.Code
+                (fun maybeCategorySelection ->
+                    maybeCategorySelection
+                    |> Option.map
+                        (fun categorySelection ->
+                            { categorySelection with
+                                  ShouldExclude = not categorySelection.ShouldExclude }))
+
+        { model with
+              Search =
+                  { model.Search with
+                        MetadataSelection = newMetadataSelection } },
+        Cmd.none
     | SelectItem (category, selectedOption) ->
         let newCategorySelection =
             // Find the already selected values for this category, if any, and append the new one
@@ -39,37 +66,6 @@ let update (msg: Msg) (model: LoadedCorpusModel) : LoadedCorpusModel * Cmd<Msg> 
                             MetadataSelection = newSelection } }
 
         newModel, Cmd.none
-    | ToggleExclude category ->
-        let newMetadataSelection =
-            model.Search.MetadataSelection
-            |> Map.change
-                category.Code
-                (fun maybeCategorySelection ->
-                    maybeCategorySelection
-                    |> Option.map
-                        (fun categorySelection ->
-                            { categorySelection with
-                                  ShouldExclude = not categorySelection.ShouldExclude }))
-
-        { model with
-              Search =
-                  { model.Search with
-                        MetadataSelection = newMetadataSelection } },
-        Cmd.none
-    | ToggleMetadataMenuOpen category ->
-        let newCode =
-            if model.OpenMetadataCategoryCode = Some category.Code then
-                None
-            else
-                Some category.Code
-
-        { model with
-              OpenMetadataCategoryCode = newCode },
-        Cmd.none
-    | ToggleShowSelectionOpen ->
-        { model with
-              IsShowSelectionOpen = not model.IsShowSelectionOpen },
-        Cmd.none
     | DeselectItem (category, optionToRemove) ->
         let newSelection =
             // Find the selected values for this category and remove the given one
@@ -94,3 +90,7 @@ let update (msg: Msg) (model: LoadedCorpusModel) : LoadedCorpusModel * Cmd<Msg> 
                             MetadataSelection = newSelection } }
 
         newModel, Cmd.none
+    | ToggleShowSelectionOpen ->
+        { model with
+              IsShowSelectionOpen = not model.IsShowSelectionOpen },
+        Cmd.none
