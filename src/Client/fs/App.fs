0fb2b0b250c310e32a9c234942b37c8be2477a2a
Anders Nøklestad <anders.noklestad@iln.uio.no>
2021-10-14 14:51:38 +0200

    Integrate jPlayer for now
    Copied over from earlier Glossa versions. A better solution would be to implement something more suitable for React-based applications than the jQuery-based jPlayer and its accompanying TextBox.

--- src/Client/fs/App.fs
+++ src/Client/fs/App.fs
@@ -8,6 +8,12 @@ open Elmish.Debug
 open Elmish.HMR
 #endif
 
+// TODO: Replace the jQuery-based jPlayer and the accompanying TextBox defined in
+// jplayer.js with something more suitable for React-based applications!
+Fable.Core.JsInterop.importSideEffects ("../../../node_modules/jquery/dist/jquery.js")
+Fable.Core.JsInterop.importSideEffects ("../jquery.jplayer.js")
+Fable.Core.JsInterop.importSideEffects ("../jplayer.js")
+
 Program.mkProgram Update.init Update.update View.Index.MainView
 #if DEBUG
 |> Program.withConsoleTrace
