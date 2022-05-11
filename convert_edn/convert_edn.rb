95bd96cebc4d56a63ff980e541255a3f6f5ac484
Anders Nøklestad <anders.noklestad@iln.uio.no>
2022-05-11 19:29:14 +0200

    More WIP on grammatical menu conversion script

--- convert_edn/convert_edn.rb
+++ convert_edn/convert_edn.rb
@@ -11,32 +11,56 @@ File.open("/Users/noklesta_adm/prosjekter/lein/cglossa_cljs/resources/attributes
     attr_name = pos[1]
     cats = pos[3].each_slice(2).to_a
 
-    if pos_index == 0 then
-        puts "        [ (pos,"
-    else
-        puts "          (pos,"
-    end
-
-    puts "           \"#{attr_value}\","
-    puts "           \"#{attr_name}\","
-
-    cats.each_with_index do |cat, cat_index|
-        cat_name = cat[0]
-        if cat_index == 0 then
-            puts "           [ (\"#{cat_name}\","
+    if cats.size > 0 then
+        if pos_index == 0 then
+            puts "        [ (pos,"
         else
-            puts "             (\"#{cat_name}\","
+            puts "          (pos,"
         end
-        cat_values = cat[1]
-        cat_values.each_with_index do |cat_value, cat_value_index|
-            cat_value_name, cat_value_value, cat_value_text = cat_value
-            if cat_value_index == 0 then
-                puts "              [ (#{cat_value_name}, \"#{cat_value_value}\", \"#{cat_value_text}\")"
-            elsif cat_value_index == cat_values.size - 1 then
-                puts "                (#{cat_value_name}, \"#{cat_value_value}\", \"#{cat_value_text}\") ])"
+
+        puts "           \"#{attr_value}\","
+        puts "           \"#{attr_name}\","
+
+        cats.each_with_index do |cat, cat_index|
+            cat_name = cat[0]
+            cat_values = cat[1]
+            if cat_values.size == 1 then
+                cva, cat_value_value, cat_value_text = cat_values[0]
+                cat_value_attr_name = if cva == :type then "``type``" else cva end
+                if cat_index == 0 then
+                    puts "           [ (\"#{cat_name}\", [ (#{cat_value_attr_name}, \"#{cat_value_value}\", \"#{cat_value_text}\") ]) ])"
+                else
+                    puts "             (\"#{cat_name}\", [ (#{cat_value_attr_name}, \"#{cat_value_value}\", \"#{cat_value_text}\") ]) ])"
+                end
             else
-                puts "                (#{cat_value_name}, \"#{cat_value_value}\", \"#{cat_value_text}\")"
+                if cat_index == 0 then
+                    puts "           [ (\"#{cat_name}\","
+                else
+                    puts "             (\"#{cat_name}\","
+                end
+                cat_values.each_with_index do |cat_value, cat_value_index|
+                    cva, cat_value_value, cat_value_text = cat_value
+                    cat_value_attr_name = if cva == :type then "``type``" else cva end
+
+                    if cat_value_index == 0 then
+                        puts "              [ (#{cat_value_attr_name}, \"#{cat_value_value}\", \"#{cat_value_text}\")"
+                    elsif cat_value_index == cat_values.size - 1 then
+                        if cat_index == cats.size - 1 then
+                            puts "                (#{cat_value_attr_name}, \"#{cat_value_value}\", \"#{cat_value_text}\") ]) ])"
+                        else
+                            puts "                (#{cat_value_attr_name}, \"#{cat_value_value}\", \"#{cat_value_text}\") ])"
+                        end
+                    else
+                        puts "                (#{cat_value_attr_name}, \"#{cat_value_value}\", \"#{cat_value_text}\")"
+                    end
+                end
             end
+        end
+    else
+        if pos_index == pos_menu.size - 1 then
+            puts "          (pos, \"#{attr_value}\", \"#{attr_name}\", []) ]"
+        else
+            puts "          (pos, \"#{attr_value}\", \"#{attr_name}\", [])"
         end
     end
   end
