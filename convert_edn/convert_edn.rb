ddebc99c754f182a77c8c1f56c48312ec930a1dc
Anders Nøklestad <anders.noklestad@iln.uio.no>
2022-05-13 15:08:44 +0200

    Remove unnecessary 'then's

--- convert_edn/convert_edn.rb
+++ convert_edn/convert_edn.rb
@@ -11,8 +11,8 @@ File.open("/Users/noklesta_adm/prosjekter/lein/cglossa_cljs/resources/attributes
     attr_name = pos[1]
     cats = pos[3].each_slice(2).to_a
 
-    if cats.size > 0 then
-        if pos_index == 0 then
+    if cats.size > 0
+        if pos_index == 0
             puts "        [ (pos,"
         else
             puts "          (pos,"
@@ -24,16 +24,16 @@ File.open("/Users/noklesta_adm/prosjekter/lein/cglossa_cljs/resources/attributes
         cats.each_with_index do |cat, cat_index|
             cat_name = cat[0]
             cat_values = cat[1]
-            if cat_values.size == 1 then
+            if cat_values.size == 1
                 cva, cat_value_value, cat_value_text = cat_values[0]
                 cat_value_attr_name = if cva == :type then "``type``" else cva end
-                if cat_index == 0 then
+                if cat_index == 0
                     puts "           [ (\"#{cat_name}\", [ (#{cat_value_attr_name}, \"#{cat_value_value}\", \"#{cat_value_text}\") ]) ])"
                 else
                     puts "             (\"#{cat_name}\", [ (#{cat_value_attr_name}, \"#{cat_value_value}\", \"#{cat_value_text}\") ]) ])"
                 end
             else
-                if cat_index == 0 then
+                if cat_index == 0
                     puts "           [ (\"#{cat_name}\","
                 else
                     puts "             (\"#{cat_name}\","
@@ -42,10 +42,10 @@ File.open("/Users/noklesta_adm/prosjekter/lein/cglossa_cljs/resources/attributes
                     cva, cat_value_value, cat_value_text = cat_value
                     cat_value_attr_name = if cva == :type then "``type``" else cva end
 
-                    if cat_value_index == 0 then
+                    if cat_value_index == 0
                         puts "              [ (#{cat_value_attr_name}, \"#{cat_value_value}\", \"#{cat_value_text}\")"
-                    elsif cat_value_index == cat_values.size - 1 then
-                        if cat_index == cats.size - 1 then
+                    elsif cat_value_index == cat_values.size - 1
+                        if cat_index == cats.size - 1
                             puts "                (#{cat_value_attr_name}, \"#{cat_value_value}\", \"#{cat_value_text}\") ]) ])"
                         else
                             puts "                (#{cat_value_attr_name}, \"#{cat_value_value}\", \"#{cat_value_text}\") ])"
@@ -57,7 +57,7 @@ File.open("/Users/noklesta_adm/prosjekter/lein/cglossa_cljs/resources/attributes
             end
         end
     else
-        if pos_index == pos_menu.size - 1 then
+        if pos_index == pos_menu.size - 1
             puts "          (pos, \"#{attr_value}\", \"#{attr_name}\", []) ]"
         else
             puts "          (pos, \"#{attr_value}\", \"#{attr_name}\", [])"
